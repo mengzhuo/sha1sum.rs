5c6997793ad1ccd77778c486641aa8918392c4bd
Meng Zhuo <mengzhuo1203@gmail.com>
2018-08-15 18:52:32 +0800

    add check

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate crypto;
 
-use std::io::Read;
-use std::{env,fs,io};
+use std::io::{Read, BufReader, BufRead};
+use std::{env,fs,io,string,process};
 use crypto::sha1::Sha1;
 use crypto::digest::Digest;
 
@@ -10,12 +10,15 @@ Print or check SHA1 (160-bit) checksums.
 
 With no FILE, or when FILE is -, read standard input.
 
-      --help     display this help and exit
+        --help      display this help and exit
+    -c, --check     read SHA1 sums from the FILEs and check them
 ";
 
 fn main() {
 
     let mut files = Vec::new();
+    let mut check = false;
+    let mut quiet = false;
 
     for arg in env::args().skip(1) {
         match arg.as_ref() {
@@ -23,23 +26,93 @@ fn main() {
             eprintln!("{}", HELP_MSG);
             return
         },
+        "-c" | "--check" => {
+            check = true;
+        },
+        "--quiet" => {
+            quiet = true;
+        },
         _ =>  files.push(arg)
         }
+    }
+
+    if check && !files.is_empty(){
+        check_files(files, quiet);
+        return;
     }
 
     if files.is_empty() {
         files.push(String::from("-"))
     }
 
+
     for fp in files {
-        match handle_file(&fp) {
+        match sha1sum(&fp) {
             Ok(hex)=> println!("{}  {}", hex, fp),
             Err(err)=> eprintln!("sha1sum: {}: {}", fp, err),
         }
     }
 }
 
-fn handle_file(fp: &String) -> Result<String, io::Error> {
+fn parse_checksum_file(fp: &String) -> Result<(Vec<string::String>, Vec<string::String>), io::Error> {
+        let fd = fs::File::open(fp)?;
+        let mut rdr = BufReader::new(fd);
+        let mut lines = Vec::new();
+
+        let mut success = Vec::new();
+        let mut failed = Vec::new();
+
+        loop {
+            let mut line = String::new();
+            let n = rdr.read_line(&mut line)?;
+            if n == 0 {
+                break;
+            }
+            lines.push(line);
+        }
+
+        for line in lines {
+            let items:Vec<&str> = line.split_whitespace().collect();
+            if items.len() < 2 {
+                eprintln!("sha1sum: failed to parse {}", line);
+                continue
+            }
+
+            let (sha_checksum, sha_file) = (items[0], items[1]);
+            let cal_checksum = sha1sum(&String::from(sha_file))?;
+
+            if cal_checksum != sha_checksum {
+                failed.push(format!("{}: FAILED", sha_file));
+                continue
+            }
+            success.push(format!("{}: OK", sha_file));
+        }
+
+        Ok((success, failed))
+}
+
+fn check_files(files: Vec<String>, quiet: bool) {
+    for fp in files {
+       match  parse_checksum_file(&fp) {
+        Ok(result) => {
+            let (success, failed) = result;
+            if quiet && !failed.is_empty() {
+                process::exit(1);
+            }
+
+            for line in success {
+                println!("{}", line)
+            }
+            for line in failed {
+                println!("{}", line)
+            }
+        },
+        Err(err)=> eprintln!("sha1sum: {}: {}", fp, err),
+       }
+    }
+}
+
+fn sha1sum(fp: &String) -> Result<String, io::Error> {
     let mut hasher = Sha1::new();
 
     let mut f:Box<io::Read> = match fp.as_ref() {
